62457466b889897185975389dee424472253d6d9
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-05-04 08:09:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,66 @@
 //your JS code here. If required.
+/*  your css code here. If applicable */
+body{
+    margin:0;
+    padding: 0;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.container{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    gap:1rem;
+}
+#fb{
+    color:blue;
+    font-size:3rem;
+    font-weight: bold;
+}
+#desc{
+    font-size: 1.2rem;
+    font-weight: bold;
+}
+form{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    padding: 1rem;
+    width:300px;
+    height:300px;
+}
+form input{
+    width:100%;
+    padding: 0.5rem;
+    font-weight: bold;
+}
+#login{
+    width:100%;
+    padding: 0.8rem;
+    border: none;
+    background-color: rgb(78, 78, 250);
+    color:white;
+    font-weight: bold;
+    cursor: pointer;
+}
+a{
+    text-decoration: none;
+    font-size: 1.2rem;
+}
+#create{
+    width: 65%;
+    padding: 0.8rem;
+    border: none;
+    border-radius: 0.5rem;
+    background-color: rgb(27, 238, 27);
+    color:white;
+    font-weight: bold;
+    font-size: 1rem;
+    cursor: pointer;
+}
+.login-form{
+    box-shadow: 0 0 20px #ccc;
+}
